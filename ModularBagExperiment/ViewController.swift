2d13a3b020cd19cad850888ea2815fd950358022
balapoq <bala@poqcommerce.com>
2018-04-03 16:34:37 +0100

    Initial Commit

---
+++ ModularBagExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModularBagExperiment
+//
+//  Created by Balaji Reddy on 03/04/2018.
+//  Copyright © 2018 Balaji Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
